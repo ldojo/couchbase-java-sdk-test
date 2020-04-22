f3ff6f303350e843601faabee72eace15f7cf848
Lev Shulman <lshulman@redhat.com>
2020-04-21 21:36:19 -0600

    wrapped the test in a try/catch

--- src/main/java/com/citi/couchbase/TestCouchbase.java
+++ src/main/java/com/citi/couchbase/TestCouchbase.java
@@ -9,22 +9,28 @@ public class TestCouchbase {
 
 	public static void main(String[] args) {
 
-		if(args.length == 0) {
+		if (args.length == 0) {
 			System.out.println("Ex: java -jar target/couchbase-test.jar <host> <username> <password> <bucket-name>");
 			System.exit(1);
 		}
-		
-		Cluster cluster = Cluster.connect(args[0], args[1], args[2]);
-		Bucket bucket = cluster.bucket(args[3]);
-		Collection collection = bucket.defaultCollection();
 
-		MutationResult upsertResult = collection.upsert("my-document", JsonObject.create().put("name", "mike"));
-		GetResult getResult = collection.get("my-document");
-		System.out.println(getResult);
+		try {
 
-		QueryResult result = cluster.query("select \"Hello World\" as greeting");
-		System.out.println(result.rowsAsObject());
-		System.out.println("couchbase test was successful!");
+			Cluster cluster = Cluster.connect(args[0], args[1], args[2]);
+			Bucket bucket = cluster.bucket(args[3]);
+			Collection collection = bucket.defaultCollection();
+
+			MutationResult upsertResult = collection.upsert("my-document", JsonObject.create().put("name", "mike"));
+			GetResult getResult = collection.get("my-document");
+			System.out.println(getResult);
+
+			QueryResult result = cluster.query("select \"Hello World\" as greeting");
+			System.out.println(result.rowsAsObject());
+			System.out.println("couchbase test was successful!");
+		} catch (Exception e) {
+			System.out.println("failed Couchbase connection test with exception " + e + ": " + e.getMessage());
+			System.exit(1);
+		}
 	}
 
 }
